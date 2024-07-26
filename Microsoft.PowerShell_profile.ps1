c6ba0b0c6d1c1cb0879455efdf2abfedad34f192
Remy Blok <remyblok@remyblok.nl>
2024-07-26 14:32:56 +0200

    Commit Posh config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+function IsInteractive {
+    # not including `-NonInteractive` since it apparently does nothing
+    # "Does not present an interactive prompt to the user" - no, it does present!
+    $non_interactive = '-command', '-c', '-encodedcommand', '-e', '-ec', '-file', '-f'
+
+    # alternatively `$non_interactive [-contains|-eq] $PSItem`
+    -not ([Environment]::GetCommandLineArgs() | Where-Object -FilterScript {$PSItem -in $non_interactive})
+}
+
+if (IsInteractive) {
+	oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
+}
